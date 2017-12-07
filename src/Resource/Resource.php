02d7b36914166470c7c5e432e5052a8fbc8f35dc
Rory Kelly <rorykelly76@gmail.com>
2017-12-07 19:57:33 +0000

    Implemented class properties and constructor

--- src/Resource/Resource.php
+++ src/Resource/Resource.php
@@ -2,8 +2,18 @@
 
 namespace Halogen\Resource;
 
+use Illuminate\Database\Eloquent\Model;
+
 class Resource {
-	
-	
-	
+		
+	protected $rel;
+	protected $model;
+
+	public function __construct($rel, Model $model) {
+
+		$this->rel = $rel;
+		$this->model = $model;
+
+	}
+
 }
