bdf1da8465ad94e7c847c2e1d35a1eca2b7b7c91
Rory Kelly <rorykelly76@gmail.com>
2017-12-17 01:41:16 +0000

    Implemented addLink function

--- src/Link/HasLinks.php
+++ src/Link/HasLinks.php
@@ -4,6 +4,18 @@ namespace Halogen\Link;
 
 trait HasLinks {
 
+	private $links;
 
+	public function addLink($rel, $href) {
+
+		if(!in_array('_links', $this->appends))
+			array_push($this->appends, '_links');
+
+		if(array_key_exists($rel, $links))
+			$links[$rel] = $href;
+		else
+			$links = array_merge($links, [$rel => $href]);
+
+	}
 
 }
