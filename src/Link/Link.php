0a38f2488f4abc0163c316de65670278cb703b05
Rory Kelly <rorykelly76@gmail.com>
2017-12-17 05:50:07 +0000

    Implemented search and replace templating for Link class

--- src/Link/Link.php
+++ src/Link/Link.php
@@ -6,15 +6,21 @@ class Link implements \JsonSerializable {
 	
 	protected $rel;
 	protected $href;
+	protected $replace;
 
-	public function __construct($rel, $href) {
+	public function __construct($rel, $href, $replace) {
 
 		$this->rel = $rel;
 		$this->href = $href;
+		$this->replace = $replace;
 
 	}
 
 	public function jsonSerialize() {
+
+		foreach($replace as $key => $val) {
+			$this->href = str_replace($key, $val, $this->href);
+		}
 
 		return [$this->rel => ['href' => $this->href]];
 
